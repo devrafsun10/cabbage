bfa8783397a0ea4b9a72456221bae25e48011fbd
devrafsun10 <ssiyam152@gmail.com>
2026-02-01 00:46:18 +0600

    banner-responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,22 +6,22 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <div className='bg-[#F7F5EB] py-29.75 relative'>
-        <div className=' container flex justify-between items-center z-10'>
+    <div className='bg-[#F7F5EB] md:py-29.75 relative pt-29.75 p-5'>
+        <div className=' container md:flex md:justify-between md:items-center z-10'>
             <div>
-                <div className='flex  items-center mb-2.25'>
+                <div className='flex  items-center md:mb-2.25'>
                     <img src={herb} alt="#herb" />
                     <p className='font-secondary font-normal text-[18px] text-[#232323]'>100% genuine Products</p>
                 </div>
-                <h1 className='font-primary font-bold text-[70px] text-[#232323] mb-8.75'>Tasty & Healthy <span>organic Food</span></h1>
+                <h1 className='font-primary font-bold md:text-[70px] text-[34px] text-[#232323] md:mb-8.75 mb-5'>Tasty & Healthy <span>organic Food</span></h1>
 
-                 <button className='py-4 px-7.5 bg-[#80B500] font-secondary font-bold text-[14px] text-[#FFFFFF] flex items-center' >Explore Products
+                 <button className='md:py-4 py-2 md:px-7.5 px-6 bg-[#80B500] font-secondary font-bold text-[10px] md:text-[14px] text-[#FFFFFF] flex items-center' >Explore Products
                     <FaLongArrowAltRight className='ml-1' />
                  </button>
             </div>
 
             <div>
-                <img src={banner} alt="#banner" />
+                <img className='md:w-full w-90' src={banner} alt="#banner" />
             </div>
         </div>
         <img src={leaf} alt="" className=' absolute top-0 left-0 w-20'/>
